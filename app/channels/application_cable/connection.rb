be621cc2a5a6d82a74b2bfee3bd754aaaf104758
Marcos Scorzoni <mlessa@gmail.com>
2018-09-20 18:53:30 +0100

    setup action cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,10 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    indentified_by :current_user
+
+    def connect
+      self.current_user = request.session.fetch('username', nil)
+      reject_unathorized_connection unless current_user
+    end
   end
 end
---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,16 @@
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+  end
+
+  def send_message(payload)
+    message = Message.new(author: current_user, text: payload['message'])
+
+    ActionCable.server.broadcast 'chat', message: render(message) if message.save
+  end
+
+  private
+
+  def render(message)
+    ApplicationController.new.helpers.c('message', message: message)
+  end
+end
